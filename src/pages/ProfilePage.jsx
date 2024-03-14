95a3cc06ef6007a75d052614e286f66512f7e11d
NARARIA03 <hyeonseong0305@gmail.com>
2024-03-15 08:11:14 +0900

    API 연결 성공

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { uidState } from "../recoil/Atoms";
 import { useRecoilValue } from "recoil";
+import axios from "axios";
+
+import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 
 const ProfilePage = () => {
   const uid = useRecoilValue(uidState);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const testGetProfile = async (uid) => {
+    setLoading(true);
+    try {
+      const API_URL =
+        "https://cors-anywhere.herokuapp.com/" +
+        "https://enka.network/api/uid/" +
+        uid +
+        "/";
+      const data = await axios.get(API_URL);
+      setUserData(data.data);
+    } catch {
+      // 오류 발생시 실행
+      console.error("error");
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    testGetProfile(uid);
+  }, []);
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <p className="text-3xl">Profile Page</p>
-      <p className="text-3xl">{uid}</p>
-    </div>
+    <>
+      <Navbar />
+      <div className="h-screen flex items-center justify-center bg-gray-800">
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <p className="text-3xl">Profile Page</p>
+            <p className="text-3xl">{userData?.playerInfo?.nickname}</p>
+            <p className="text-3xl">{uid}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
