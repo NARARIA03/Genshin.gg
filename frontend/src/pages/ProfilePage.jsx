af72640f7b478186020b22320056d269ff987dde
NARARIA03 <hyeonseong0305@gmail.com>
2024-05-05 03:43:21 +0900

    design: 프로필페이지의 예외 상황 안내 반응형 처리
    "캐릭터 상세보기가 비공개 상태이거나..." 멘트와
    "UID 검색에 실패했습니다..." 멘트를
    모바일 기기에서도 편하게 볼 수 있도록 반응형으로 바꿔줬다.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -33,16 +33,26 @@ export default function ProfilePage() {
               <Characters />
             </div>
           ) : (
-            <div className="flex justify-center items-center">
-              <p className=" text-white text-xl">
-                캐릭터 상세보기가 비공개 상태이거나, 진열장에 전시된 캐릭터가
-                없습니다. 인게임에서 확인 부탁드립니다!
+            <div className="flex justify-center flex-col items-center">
+              {/* 태블릿, 핸드폰에서 보일 메시지 */}
+              <p className="md:hidden text-white text-base mx-7 my-2">
+                원신 게임 내에서 캐릭터 상세보기가 비공개 상태이거나, 진열장에
+                전시된 캐릭터가 없어서 정보를 불러올 수 없습니다. 인게임에서
+                확인 부탁드립니다!
               </p>
+              {/* 노트북, 데스크탑에서 보일 메시지 */}
+              <div className="hidden md:flex justify-center items-center flex-col text-white text-2xl mx-7 my-1">
+                <p>원신 게임 내에서 캐릭터 상세보기가 비공개 상태이거나,</p>
+                <p>
+                  진열장에 전시된 캐릭터가 없어서 정보를 불러올 수 없습니다.
+                </p>
+                <p>인게임에서 확인 부탁드립니다!</p>
+              </div>
             </div>
           )
         ) : (
           <div className="flex justify-center items-center">
-            <p className=" text-white text-xl">
+            <p className=" text-white text-sm md:text-base lg:text-xl mx-5">
               UID 검색에 실패했습니다. 메인 화면에서 다시 시도해주세요!
             </p>
           </div>
