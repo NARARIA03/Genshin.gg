3940a165289f442c7c1d97c2fded55da7cd17133
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-27 18:52:45 +0900

    refactor: Footer 멘트 수정
    저작권 관련 명시 추가

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,8 +5,6 @@ export default function Footer(): React.JSX.Element {
     <footer className="w-full bg-gray-800">
       <div className="w-[98%] flex flex-col justify-center items-center border-t border-gray-700 mx-auto">
         <div className="flex flex-col p-2">
-          <p className="text-gray-300 px-2 py-1">이 웹페이지는 취미로 개발되었으며, 상업적 용도가 아닙니다.</p>
-          <p className="text-sm text-gray-300 p-2">Developed by CHS</p>
           <a href="https://github.com/EnkaNetwork/API-docs" className="text-gray-300 hover:text-gray-400 p-2">
             <div className="flex">
               <img className="w-6 h-6 mr-2" src="/enka.png" alt="Enka.network Logo" />
@@ -16,9 +14,13 @@ export default function Footer(): React.JSX.Element {
           <a href="https://github.com/NARARIA03/Genshin.gg" className="text-gray-300 hover:text-gray-400 p-2">
             <div className="flex">
               <img className="w-6 h-6 mr-2" src="https://cdn-icons-png.flaticon.com/256/25/25231.png" alt="Github Logo" />
-              <p>Github repo</p>
+              <p>Github</p>
             </div>
           </a>
+          <p className="text-gray-400 px-2 py-1">
+            Genshin.gg는 HoYoverse와 연관이 없습니다. 이 웹페이지는 취미로 개발되었으며, 상업적 용도가 아닙니다.
+          </p>
+          <p className="text-gray-400 px-2 py-1">Genshin Impact, 게임의 콘텐츠와 소재의 트레이드마크와 저작권은 HoYoverse에 있습니다.</p>
         </div>
       </div>
     </footer>
