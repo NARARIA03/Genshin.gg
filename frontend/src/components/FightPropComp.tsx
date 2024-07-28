c183f63082e4659a46b1587d480ea9c8195d22df
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-29 01:06:47 +0900

    chore: svg 파일 class 제거, fill current로 변경

---
+++ frontend/src/components/FightPropComp.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { CharFightStats } from "../types/fightPropTypes";
+
+export default function FightPropComponent(charFightStatsList: CharFightStats[]): React.JSX.Element {}
